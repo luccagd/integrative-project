0ac01d2d30f473848d62f30f86752c6efc5b05de
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2022-01-26 17:57:43 -0300

    Início do Requisito 01: ml-insert-batch-in-fulfillment-warehouse-01

---
+++ src/test/java/com/meli/bootcamp/integrativeproject/IntegrativeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.bootcamp.integrativeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrativeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
