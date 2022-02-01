c5a3e332c0c6679f3f787c64300964acb821ec46
henrique fernandes yoshida <henrique.fernandes@mercadolivre.com>
2022-02-01 11:00:31 -0300

    fix: cartproduct repository

--- src/main/java/com/meli/bootcamp/integrativeproject/controller/PurchaseOrderController.java
+++ src/main/java/com/meli/bootcamp/integrativeproject/controller/PurchaseOrderController.java
@@ -1,21 +1,11 @@
 package com.meli.bootcamp.integrativeproject.controller;
 
-import com.meli.bootcamp.integrativeproject.dto.request.ProductRequestDTO;
 import com.meli.bootcamp.integrativeproject.dto.request.PurchaseOrderRequest;
-import com.meli.bootcamp.integrativeproject.dto.response.CartProductDto;
-import com.meli.bootcamp.integrativeproject.entity.CartProduct;
-import com.meli.bootcamp.integrativeproject.entity.Product;
 import com.meli.bootcamp.integrativeproject.service.CartProductService;
 import com.meli.bootcamp.integrativeproject.service.CartService;
 import com.meli.bootcamp.integrativeproject.service.PurchaseOrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("/fresh-products")
--- src/main/java/com/meli/bootcamp/integrativeproject/service/CartProductService.java
+++ src/main/java/com/meli/bootcamp/integrativeproject/service/CartProductService.java
@@ -1,16 +1,9 @@
 package com.meli.bootcamp.integrativeproject.service;
-
-import com.meli.bootcamp.integrativeproject.dto.response.CartProductDto;
-import com.meli.bootcamp.integrativeproject.entity.Cart;
-import com.meli.bootcamp.integrativeproject.entity.CartProduct;
 import com.meli.bootcamp.integrativeproject.repositories.CartProductRepository;
-import com.meli.bootcamp.integrativeproject.repositories.CartRepository;
-import com.meli.bootcamp.integrativeproject.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.meli.bootcamp.integrativeproject.repositories.CartProductRepository.CartProductDto;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
