69c6bcef8dbd05acb9089b78ae4d57fb10c1080d
Lemuel Coelho Zara <lemuel.czara@mercadolivre.com>
2022-02-02 16:05:19 -0300

    test: should be throw if warehouse not exists when try save

--- src/test/java/com/meli/bootcamp/integrativeproject/unit/InboundOrderServiceTest.java
+++ src/test/java/com/meli/bootcamp/integrativeproject/unit/InboundOrderServiceTest.java
@@ -1,8 +1,10 @@
 package com.meli.bootcamp.integrativeproject.unit;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import com.meli.bootcamp.integrativeproject.dto.request.InboundOrderRequestDTO;
+import com.meli.bootcamp.integrativeproject.entity.Seller;
 import com.meli.bootcamp.integrativeproject.exception.NotFoundException;
 import com.meli.bootcamp.integrativeproject.repositories.InboundOrderRepository;
 import com.meli.bootcamp.integrativeproject.repositories.SellerRepository;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 public class InboundOrderServiceTest {
 
@@ -46,7 +50,27 @@ public class InboundOrderServiceTest {
         assertEquals("Seller not found for the given id", exception.getMessage());
     }
 
+    @Test
+    public void shouldBeThrowIfWarehouseNotExistsWhenTrySave() {
+        var sellerIdExistent = 1L;
+        var agentId = 1L;
+        var request = makeFakeInboundOrderRequestDTO();
+        request.setSellerId(sellerIdExistent);
+
+        var seller = makeFakeSeller();
+
+        when(sellerRepository.findById(sellerIdExistent)).thenReturn(Optional.of(seller));
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> service.save(request, agentId));
+
+        assertEquals("Warehouse not found for the given id", exception.getMessage());
+    }
+
     private InboundOrderRequestDTO makeFakeInboundOrderRequestDTO() {
         return new InboundOrderRequestDTO();
+    }
+
+    private Seller makeFakeSeller() {
+        return new Seller();
     }
 }
