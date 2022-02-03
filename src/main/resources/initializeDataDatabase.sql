b391b0e0b9666252342e41396e1389abe184f64e
Lemuel Coelho Zara <lemuel.czara@mercadolivre.com>
2022-02-03 14:07:36 -0300

    test: should be returns 400 if warehouse not found when try save

--- src/main/resources/initializeDataDatabase.sql
+++ src/main/resources/initializeDataDatabase.sql
@@ -1,34 +1,19 @@
-INSERT INTO warehouses(name)
-VALUES ('Armazém de São Paulo');
-INSERT INTO warehouses(name)
-VALUES ('Armazém da Bahia');
+INSERT INTO warehouses(name) VALUES ('Armazém de São Paulo');
+INSERT INTO warehouses(name) VALUES ('Armazém da Bahia');
 
-INSERT INTO seller (name)
-VALUES ('Sadia');
-INSERT INTO seller (name)
-VALUES ('Procter & Gamble');
+INSERT INTO seller (name) VALUES ('Sadia');
+INSERT INTO seller (name) VALUES ('Procter & Gamble');
 
-INSERT INTO agents(name, warehouse_id)
-VALUES ('Lucca', 1);
-INSERT INTO agents(name, warehouse_id)
-VALUES ('João', 2);
+INSERT INTO agents(name, warehouse_id) VALUES ('Lucca', 1);
+INSERT INTO agents(name, warehouse_id) VALUES ('João', 2);
 
-INSERT INTO sections(category)
-VALUES ('REFRIGERADO');
-INSERT INTO sections(category)
-VALUES ('CONGELADO');
-INSERT INTO sections(category)
-VALUES ('FRESCO');
+INSERT INTO sections(category) VALUES ('REFRIGERADO');
+INSERT INTO sections(category) VALUES ('CONGELADO');
+INSERT INTO sections(category) VALUES ('FRESCO');
 
-INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id)
-VALUES (100, 0, 1, 1);
-INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id)
-VALUES (100, 0, 2, 1);
-INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id)
-VALUES (100, 0, 3, 1);
-INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id)
-VALUES (100, 0, 1, 2);
-INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id)
-VALUES (100, 0, 2, 2);
-INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id)
-VALUES (100, 0, 3, 2);
+INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 1, 1);
+INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 2, 1);
+INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 3, 1);
+INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 1, 2);
+INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 2, 2);
+INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 3, 2);
--- src/test/java/com/meli/bootcamp/integrativeproject/unit/integration/InboundOrderControllerTest.java
+++ src/test/java/com/meli/bootcamp/integrativeproject/unit/integration/InboundOrderControllerTest.java
@@ -5,23 +5,57 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 import com.meli.bootcamp.integrativeproject.repositories.SellerRepository;
+import com.meli.bootcamp.integrativeproject.repositories.WarehouseRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-@Sql(scripts = "/initializeDataDatabase.sql")
 @ActiveProfiles("integration_tests")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class InboundOrderControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeAll
+    public void beforeAll() {
+        jdbcTemplate.execute("INSERT INTO warehouses(name) VALUES ('Armazém de São Paulo')");
+        jdbcTemplate.execute("INSERT INTO warehouses(name) VALUES ('Armazém da Bahia')");
+
+        jdbcTemplate.execute("INSERT INTO seller (name) VALUES ('Sadia')");
+        jdbcTemplate.execute("INSERT INTO seller (name) VALUES ('Procter & Gamble')");
+
+        jdbcTemplate.execute("INSERT INTO agents(name, warehouse_id) VALUES ('Lucca', 1)");
+        jdbcTemplate.execute("INSERT INTO agents(name, warehouse_id) VALUES ('João', 2)");
+
+        jdbcTemplate.execute("INSERT INTO sections(category) VALUES ('REFRIGERADO')");
+        jdbcTemplate.execute("INSERT INTO sections(category) VALUES ('CONGELADO')");
+        jdbcTemplate.execute("INSERT INTO sections(category) VALUES ('FRESCO')");
+
+        jdbcTemplate.execute("INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 1, 1)");
+        jdbcTemplate.execute("INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 2, 1)");
+        jdbcTemplate.execute("INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 3, 1)");
+        jdbcTemplate.execute("INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 1, 2)");
+        jdbcTemplate.execute("INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 2, 2)");
+        jdbcTemplate.execute("INSERT INTO warehouses_sections(size, total_products, section_id, warehouse_id) VALUES (100, 0, 3, 2)");
+    }
 
     @Test
     public void shouldBeReturns404IfSellerNotFoundWhenTrySave() throws Exception {
@@ -32,7 +66,6 @@ public class InboundOrderControllerTest {
                         .header("agentId", 1)
                         .content(httpRequest)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("Seller not found for the given id"));
     }
@@ -46,8 +79,22 @@ public class InboundOrderControllerTest {
                         .header("agentId", 1)
                         .content(httpRequest)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.message").value("Warehouse not found for the given id"));;
+                .andExpect(jsonPath("$.message").value("Warehouse not found for the given id"));
+        ;
+    }
+
+    @Test
+    public void shouldBeReturns400IfAgentNotBelongToWarehouseWhenTrySave() throws Exception {
+        String httpRequest = "{\"sectionId\":1,\"warehouseId\":1,\"sellerId\":1,\"batchStock\":{\"products\":[{\"name\":\"PEIXE\",\"currentTemperature\":10,\"minimalTemperature\":5,\"quantity\":1,\"dueDate\":\"25-02-2022\",\"category\":\"CONGELADO\",\"price\":20.00},{\"name\":\"FRANGO\",\"currentTemperature\":10,\"minimalTemperature\":5,\"quantity\":10,\"dueDate\":\"15-03-2022\",\"category\":\"CONGELADO\",\"price\":10.00}]}}";
+
+        this.mockMvc
+                .perform(post("/fresh-products/inboundorder")
+                        .header("agentId", 999)
+                        .content(httpRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Agent does not belong to the given warehouse"));
+
     }
 }
